9470d92afa819430dc7e8701a5d174e74b33fe59
AndreyKhitrov <hitrov@mts.ru>
2020-04-19 20:40:52 +0300

    HW01 gradle

--- hw01-gradle/src/main/java/ru/mts/HelloOtus.java
+++ hw01-gradle/src/main/java/ru/mts/HelloOtus.java
@@ -7,7 +7,7 @@ public class HelloOtus {
 
     public static void main(String[] args) {
         List<String> str = Splitter.on("+")
-                .splitToList("This is first string+second string");
+                .splitToList("That is first string+second string");
 
         System.out.println(new StringBuilder().append("Proverka:").append(str).toString());
     }
